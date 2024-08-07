a131371b0ebfc0e67aeaa6a0914381169348297d
roshni <roshni.23545@sscbs.du.ac.in>
2024-08-08 00:35:52 +0530

    #1 resolved

--- actions/db.ts
+++ actions/db.ts
@@ -23,3 +23,42 @@ export async function createPost({username, content}: {username:string,content:s
         }
     })
 }
+export async function existLike({username, pid}:{username: string, pid: string}){
+    return await prisma.like.findFirst({
+        where : { 
+            username, 
+            pid,
+        }
+    })
+
+}
+export async function newLike({username, pid}:{username: string, pid: string}){
+
+    const exist = await existLike({username, pid})
+    if(exist){
+        return null
+    }
+    return await prisma.like.create({
+        data: {username, pid}
+    }) 
+}
+export async function countLike(pid: string){
+    const res = await prisma.like.count({
+        where:{
+            pid 
+        }
+    })
+    return res
+}
+export async function disLike({username, pid}:{username: string, pid: string}){
+    const exist = await existLike({username, pid})
+    const lid = exist?.lid
+    if(!exist){
+        return null
+    }
+    return await prisma.like.delete({
+        where:{
+            lid
+        }
+    })
+}
--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import { Card, CardDescription, CardFooter, CardHeader } from './ui/card'
 import { Button } from './ui/button'
 import { Posts } from '@prisma/client'
+import { countLike, disLike, existLike, newLike } from '@/actions/db'
+import { useUser } from '@clerk/nextjs'
 
 function Quote({data}:{data: Posts}) {
+  const [isLike, setIsLike] = useState<Boolean>(false)
+  const [count, setCount] = useState<Number>(0)
+  const {user} = useUser() 
+  async function toggleLike(){
+    const username = user?.username as string
+    const pid = data.pid
+    setIsLike((like)=>!like)
+    if(!isLike){
+      const res = await newLike({username, pid})
+      if(res){
+        await postCount()
+      }
+    }else{
+      const res = await disLike({username, pid})
+      if(res){
+        await postCount()
+      }
+    }
+    
+  }
+  async function postCount(){
+    const res = await countLike(data.pid)
+    setCount(res)
+  }
+  useEffect(()=>{
+    async function likeStatus(){
+      const username = user?.username as string
+      const pid = data.pid
+      const res = await existLike({username, pid})
+      if(res){
+        setIsLike(true)
+      }
+    }
+    likeStatus()
+    postCount()
+  },[])
+
   return (
     <Card className='shadow-lg w-full'>
         <CardHeader className='flex justify-between items-center text-xs p-4'>
@@ -14,7 +54,8 @@ function Quote({data}:{data: Posts}) {
            {data.content}
         </CardDescription>
         <CardFooter className='w-full p-4 '>
-            <Button className='w-full'>Like</Button>
+            <Button onClick={toggleLike} variant={isLike ? 'destructive':'default'} className='w-full'>Like</Button>
+            <h1 className='items-center p-2 min-w-fit'>{count.toString()} Likes</h1>
         </CardFooter>
     </Card>
   )
